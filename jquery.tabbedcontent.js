73522e52c37bbb89e3eb6cb9428d5917896d811b
Òscar Casajuana <elboletaire@underave.net>
2013-03-21 00:30:17 +0100

    First commit

---
+++ jquery.tabbedcontent.js
@@ -0,0 +1,137 @@
+/* Tabs plugin for jQuery created by Oscar Casajuana < elboletaire at underave dot net > */
+(function($) {
+	var Tabbedcontent = function(tabcontent, options)
+	{
+		var defaults = {
+			links 		  : tabcontent.prev().find('a'), // the tabs itself. By default is selected the previous element of the tabcontent selector
+			errorSelector : '.error-message', // false to disable
+			speed		  : false, // speed of the show effect. Set to null or false to disable
+			onSwitch	  : false, // onSwitch callback
+			onInit		  : false, // onInit callback
+			currentClass  : 'current', // current selected tab class (is set to the <a> element)
+			historyState  : 'tabbed' // nothing to worry about..
+		}, 
+		saveHistory = true,
+		firstTime = true;
+
+		options = $.extend(defaults, options);
+
+		links = options.links;
+
+		if (!links.version) {
+			links = $(links);
+		}
+
+		var children = tabcontent.children(),
+			_this = this;
+
+		function init()
+		{
+			// Switch to "first" tab
+			if (tabExists(document.location.hash))
+			{
+				_this.switch(document.location.hash);
+			}
+			else if (options.errorSelector && children.find(options.errorSelector).length)
+			{
+				children.each(function() {
+					if ($(this).find(options.errorSelector).length)
+					{
+						_this.switch("#" + $(this).attr("id"));
+						return false;
+					}
+				});
+			}
+			else
+			{
+				_this.switch("#" + tabcontent.children(":first-child").attr("id"));
+			}
+
+			// Bindings
+			links.bind("click", function(e) {
+				_this.switch($(this).attr('href'));
+				if (typeof history != 'undefined' && ('pushState' in history)) {
+					e.preventDefault();
+				}
+			});
+
+			if ('onpopstate' in window) {
+				window.onpopstate = hashSwitch;
+			} else {
+				$(window).bind('hashchange', hashSwitch);
+			}
+
+			// onInit callback
+			if (options.onInit && typeof options.onInit == 'function') {
+				options.onInit();
+			}
+		}
+
+		function hashSwitch(e) {
+			if (e.state == options.historyState) {
+				saveHistory = false;
+			}
+			if (tabExists(document.location.hash)) {
+				_this.switch(document.location.hash);
+			}
+		}
+
+		function tabExists(tab) {
+			return children.filter(tab).length ? true : false;
+		}
+
+		function onShow(tab) {
+			if (saveHistory && typeof history != 'undefined' && ('pushState' in history)) {
+				if (firstTime) {
+					firstTime = false;
+					setTimeout(function() {
+						history.replaceState(options.historyState, '', tab);
+					}, 100)
+				} else {
+					history.pushState(options.historyState, '', tab);
+				}
+			}
+			// onSwitch callback
+			if (options.onSwitch && typeof options.onSwitch == 'function') {
+				options.onSwitch(tab);
+			}
+		}
+
+		function getTabId(tabNum) {
+			return '#' + children.eq(tabNum).attr('id');
+		}
+
+		this.switch = function(tab)
+		{
+			if (tab.toString().match(/^[0-9]+$/)) {
+				tab = getTabId(tab);
+			}
+			if (!tabExists(tab)) {
+				return false;
+			}
+			links.removeClass(options.currentClass);
+			links.filter('a[href='+ tab +']').addClass(options.currentClass);
+			children.hide();
+			children.filter(tab).show(options.speed, function() {
+				if (options.speed)
+				{
+					onShow(tab);
+				}
+			});
+			if (!options.speed) {
+				onShow(tab);
+			}
+			return true;
+		}
+
+		init();
+	}
+
+	$.fn.tabbedContent = function(options) {
+		return this.each(function() {
+			var tabs = new Tabbedcontent($(this), options);
+			$(this).data('api', tabs);
+		})
+	}
+
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,108 @@
+# Easy to use jQuery tabs plugin
+jQuery TabbedContent is a lightweight tabs plugin that uses the HTML5 history API to add your tab navigation to your browser's history.
+
+It also has an API that will let you switch between tabs externally.
+
+## Usage
+
+Basic layout:
+
+```html4
+<ul>
+	<li><a href="#tab-1">Tab 1</a></li>
+	<li><a href="#tab-2">Tab 2</a></li>
+	<li><a href="#tab-3">Tab 3</a></li>
+	<li><a href="#tab-n">Tab N</a></li>
+</ul>
+<div class="tabscontent">
+	<div id="tab-1">
+		<!-- your first tab content -->
+	</div>
+	<div id="tab-2">
+		<!-- your second tab content -->
+	</div>
+	<div id="tab-3">
+		<!-- your third tab content -->
+	</div>
+	<div id="tab-n">
+		<!-- your n tab content -->
+	</div>
+</div>
+```
+
+The links of the tabs should point to each tab id.
+
+Basic javascript initialization:
+
+```javascript
+$('.tabscontent').tabbedContent();
+```
+
+By default the plugin will take the links inside the previous wrapper related to the tabs layer; but you can specify your links selector, so you can put your links everywhere:
+
+```html
+<ul class="tabs">
+	[...]
+```
+
+```javascript
+$('.tabscontent').tabbedContent({
+	links: 'ul.tabs li a' // you can also pass a jquery object containing the links
+})
+```
+
+### Error Detector
+This option is very practic if you're working with forms and want the tabs be opened on the first containing an error.
+
+Simply specify the `errorSelector` selector:
+
+```javascript
+$('.tabscontent').tabbedContent({
+	errorSelector : '.error-message'
+});
+```
+
+When the plugin inits searchs for `errorSelector` inside the tabs, opening the first tab containing it.
+
+### Callbacks
+jQuery TabbedContent has two callbacks that may be util to you: `onInit` and `onSwitch`.
+
+```javascript
+$('.tabscontent').tabbedContent({
+	onInit: function() {
+		console.log('tabs initialized');
+	},
+	onSwitch: function(tab) {
+		// Init a WYSIWYG editor on the tab (for example..)
+		if (!$(tab + ' textarea').data('wysiwyg-initialized')) {
+			initWysiwyg(tab + ' textarea');
+		}
+	}
+})
+
+### Full configuration
+
+```javascript
+$('.tabscontent').tabbedContent({
+	links 		  : '.tabs', // the tab links
+	errorSelector : '.error-message', // false to disable
+	speed		  : false, // speed of the show effect. Set to null or false to disable
+	onSwitch	  : false, // onSwitch callback
+	onInit		  : false, // onInit callback
+	currentClass  : 'current', // current selected tab class (is set to the <a> element)
+	historyState  : 'tabbed' // nothing to worry about..
+});
+```
+
+## API
+jQuery TabbedContent has a simple API that will allow you to switch between tabs. To use it simply call `data('api')`:
+
+```javascript
+var mytabs = $('.tabscontent').tabbedContent().data('api');
+
+// now you can use the switch method:
+mytabs.switch('#tab-2');
+
+// or using it's index...
+mytabs.switch(1); // note that first tab begins at 0
+```
