f05e68f5dfe5a204af7078739bbf940d3f9eb775
Òscar Casajuana <elboletaire@underave.net>
2017-05-21 16:44:59 +0200

    Preparing everything for automatic deploys to gh-pages + testing

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+if [ ${TRAVIS_BRANCH} != 'master' ]; then
+  exit 0
+fi
+
+# Set identity
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+# Update dist files
+./node_modules/.bin/gulp clean
+./node_modules/.bin/gulp
+
+# Change remote
+git remote remove origin
+git remote add origin https://${GH_TOKEN}@github.com/elboletaire/tabbedcontent.git
+git fetch origin
+# Ensure we're not on a detached branch
+git checkout master
+# Commit and push
+git add -f dist
+git commit -m "Update dist files [skip ci]"
+git push origin master
+
+# prepare gh-pages
+git clone --branch gh-pages --depth 1 \
+    https://${GH_TOKEN}@github.com/elboletaire/tabbedcontent.git \
+    ../gh-pages
+
+# remove all its content
+rm -frv ../gh-pages/demos/*
+# copy what we want
+cp -frv dist/* ../gh-pages
+
+cd ../gh-pages/
+
+# Push generated files
+git add .
+git commit -m "Update gh-pages"
+git push origin gh-pages
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,42 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename')
-;
+const gulp = require('gulp'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename'),
+      cleanCss = require('gulp-clean-css'),
+      chmod = require('gulp-chmod'),
+      clean = require('gulp-clean'),
+      replace = require('gulp-replace')
 
-gulp.task('default', function() {
-    return gulp.src('tabbedcontent.js')
-        .pipe(uglify({
-            preserveComments: 'license'
-        }))
-        .pipe(rename({
-            extname: '.min.js'
-        }))
-        .pipe(gulp.dest('.'))
-    ;
-});
+gulp.task('demos:css', () => {
+  return gulp.src('src/demos/*.css')
+    .pipe(cleanCss())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(chmod(0o664))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('assets:js', () => {
+  return gulp.src('src/tabbedcontent.js')
+    .pipe(uglify({
+      preserveComments: 'license'
+    }))
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(chmod(0o664))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('clean', () => {
+  return gulp.src('dist/*', { read: false })
+    .pipe(clean())
+})
+
+gulp.task('assets', ['assets:css', 'assets:js'], () => {
+  return gulp.src('src/index.html')
+    .pipe(replace('./tabbedcontent', './tabbedcontent.min'))
+    .pipe(chmod(0o664))
+    .pipe(gulp.dest('./dist'))
+})
+gulp.task('default', ['assets'])
